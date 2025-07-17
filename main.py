59dd447094c6858f89e8e600b2edb7dab831371f
ArilDani <m.arielardanny@gmail.com>
2025-07-17 11:53:55 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,64 @@
+print("====================================")
+print("Hello,World!") #ini akan mengeluarkan hello world di terminal
+
+print("====================================")
+#vaariabel
+a = "hello,World " #a itu adalah variabel dan hello,World itu adalah nilai
+print(a ) #ini akan mengeluarkan hello world di terminal
+
+print("====================================")
+#tipe data
+string = "Hello,World!" #data yang menggunakan single qoute atau double qoute merupakan string
+integer = 123456789 #data yang menggunakan angka merupakan integer
+float = 20.5 #data yang menggunakan angka dengan titik merupakan float
+boolean = True,False #data yang menggunakan nilai True atau False merupakan boolean
+print("data =",string,"type =",type(string))
+print("data =",integer,"type =",type(integer))
+print("data =",float,"type =",type(float))
+print("data =",boolean,"type =",type(boolean))
+
+print("====================================")
+#operasi aritmatika
+a = 10
+b = 2
+str = "hello"
+print(a + b) #menjumlahkah nilai yang berda di dalam variabel a dan b
+print(a - b) #mengurangi nilai yang berda di dalam variabel a dan b
+print(a * b) #mengkalikan nilai yang berda di dalam variabel a dan b
+print(a / b) #membagi nilai yang berda di dalam variabel a dan b
+print(a % b) #menghitung sisa bagi dari nilai yang berda di dalam variabel a dan b
+print(a ** b) #menghitung pangkat dari nilai yang berda di dalam variabel a dan b
+print(a // b) #menghitung hasil bagi dari nilai yang berda di dalam variabel a dan b
+#print(str + b) #menggabungkan string dengan integer, akan menghasilkan error karena tipe data tidak sesuai
+print(str * b) #mengulang string sebanyak nilai yang ada di dalam variabel b
+
+print("====================================")
+#operasi logika
+a = True
+b = False
+print(a and b) #menghasilkan nilai true jika kedua variabel bernilai true
+print(a or b) #menghasilkan nilai true jika salah satu variabel bernilai true
+print(not a) #menghasilkan nilai true jika variabel bernilai false
+print(not b) #menghasilkan nilai true jika variabel bernilai true
+print(a ^ b) #menghasilkan nilai true jika salah satu variabel bernilai true
+print(a >> b) #menggeser bit ke kanan
+print(a << b) #menggeser bit ke kiri
+print(a == b) #menghasilkan nilai true jika kedua variabel bernilai sama
+print(a != b) #menghasilkan nilai true jika kedua variabel bernilai tidak sama
+print(a > b) #menghasilkan nilai true jika variabel a lebih besar dari b
+print(a < b) #menghasilkan nilai true jika variabel a lebih kecil dari b
+print(a >= b) #menghasilkan nilai true jika variabel a lebih besar atau sama dengan b
+print(a <= b) #menghasilkan nilai true jika variabel a lebih kecil atau sama dengan b
+
+
+
+
+
+
+
+
+
+
+
+
+
